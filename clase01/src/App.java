baa81873b381a85b9d1aa520cd6792cc3046471c
Adn1217 <adn1217@hotmail.com>
2021-07-05 21:15:20 -0500

    Actualizar App.java

--- clase01/src/App.java
+++ clase01/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+        System.out.println("Hello, World!!!");
+        var Scanner = new Scanner(System.in);
+        System.out.println("Ingrese un número entero");
+        var numero = Scanner.nextInt();
+        var respuesta = validarNumero(numero);
+        System.out.println(respuesta);
+    }
+
+    public static String validarNumero(int numero){
+        return(numero % 2 == 0 ? numero +" es par": numero + " es impar");
+
     }
 }
